96aecf05609703ce2e407e4838b47260bb5bd3f3
Fábio <fabioalmeida132@gmail.com>
2021-09-07 15:50:37 -0300

    add MenuLink component with test

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -22,6 +22,16 @@ export const theme = {
   },
   media:{
     lteMedium: '(max-width: 786px)'
+  },
+  spacings: {
+    xsmal: '8rem',
+    small: '1.6rem',
+    medium: '2.4rem',
+    large: '3.2rem',
+    xlarge: '4.0rem',
+    xxlarge: '4.8rem',
+    huge: '5.6rem',
+    xhuge: '6.4rem',
   }
 
 }
